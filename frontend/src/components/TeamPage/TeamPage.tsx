35d98568b3f41208bf508094a8001587384bbeef
Andrey Solin <a.solin@warpa.ru>
2023-02-14 19:46:36 +0400

    team page

--- frontend/src/components/TeamPage/TeamPage.tsx
+++ frontend/src/components/TeamPage/TeamPage.tsx
@@ -1,5 +1,73 @@
+import styles from "./TeamPage.module.scss";
+import InfoCard from "@components/InfoCard";
+import testImage from "@images/test.png";
+
+const teamMembers = [
+  {
+    title: "consectetur ",
+    image: testImage,
+    description:
+      "sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+  },
+  {
+    title: "consectetur ",
+    image: testImage,
+    description:
+      "sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+  },
+  {
+    title: "consectetur ",
+    image: testImage,
+    description:
+      "sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+  },
+  {
+    title: "consectetur ",
+    image: testImage,
+    description:
+      "sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+  },
+  {
+    title: "consectetur ",
+    image: testImage,
+    description:
+      "sit amet, consectetur adipiscing elit, sed do eiusmod tempor ",
+  },
+];
+
+const LastCard = () => {
+  return (
+    <div className={styles.lastCard}>
+      <div className={styles.lastCardTitle}>Our team will be by your side</div>
+      <div className={styles.lastCardDescription}>
+        sit amet, consectetur adipiscing elit, sed do eiusmod{" "}
+      </div>
+    </div>
+  );
+};
+
 const TeamPage = () => {
-  return <div className="team">Team Page</div>
-}
+  return (
+    <div className={styles.team}>
+      <div className={styles.title}>our team</div>
+      <div className={styles.cards}>
+        {teamMembers.map((item, idx) => {
+          return (
+            <div className={styles.cardsItem}>
+              <InfoCard
+                title={item.title}
+                topTitle
+                image={item.image}
+                description={item.description}
+                key={`teammate-${idx}`}
+              />
+            </div>
+          );
+        })}
+        <LastCard />
+      </div>
+    </div>
+  );
+};
 
 export default TeamPage;
