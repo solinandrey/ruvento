6534341abe9513daf63637595b74a52e7b9ad178
Andrey Solin <a.solin@warpa.ru>
2023-03-06 16:10:33 +0400

    logos new

--- frontend/src/components/PortfolioBlock/PortfolioBlock.tsx
+++ frontend/src/components/PortfolioBlock/PortfolioBlock.tsx
@@ -9,21 +9,55 @@ import upcodes from "@images/upcodes.png";
 import revl from "@images/revl.png";
 import boom from "@images/boom.png";
 import vantageRobotics from "@images/vantage-robotics.png";
+import refocus from "@images/refocus.png";
+import alcatraz from "@images/alcatraz.png";
+import eightSleep from "@images/8sleep.png";
+import swat from "@images/swat.png";
+import elctroneek from "@images/electroneek.png";
+import reverie from "@images/reverie.png";
+import robotics from "@images/robotics.png";
+import arloid from "@images/arloid.png";
+import creopop from "@images/creopop.png";
+import powerdot from "@images/powerdot.png";
 
 interface CompanyLogo {
   logo: StaticImageData;
   alt: string;
+  link?: string;
 }
 
+// const companies: CompanyLogo[] = [
+//   { logo: mighty, alt: "Mighty Buildings" },
+//   { logo: pointOne, alt: "Point One" },
+//   { logo: prenav, alt: "Prenav" },
+//   { logo: solugen, alt: "Solugen" },
+//   { logo: upcodes, alt: "Upcodes" },
+//   { logo: revl, alt: "Revl" },
+//   { logo: boom, alt: "Boom" },
+//   { logo: vantageRobotics, alt: "Vantage Robotics" },
+// ];
+
 const companies: CompanyLogo[] = [
-  { logo: mighty, alt: "Mighty Buildings" },
-  { logo: pointOne, alt: "Point One" },
-  { logo: prenav, alt: "Prenav" },
-  { logo: solugen, alt: "Solugen" },
-  { logo: upcodes, alt: "Upcodes" },
-  { logo: revl, alt: "Revl" },
-  { logo: boom, alt: "Boom" },
-  { logo: vantageRobotics, alt: "Vantage Robotics" },
+  { logo: refocus, alt: "Refocus", link: "https://refocus.me/" },
+  { logo: alcatraz, alt: "Alcatraz AI", link: "https://www.alcatraz.ai/" },
+  {
+    logo: mighty,
+    alt: "Mighty Buildings",
+    link: "https://www.mightybuildings.com/",
+  },
+  { logo: boom, alt: "Boom", link: "https://boomsupersonic.com/" },
+  { logo: solugen, alt: "Solugen", link: "https://solugen.com/" },
+  { logo: eightSleep, alt: "Eight Sleep", link: "https://www.eightsleep.com/" },
+  { logo: swat, alt: "Swat", link: "https://www.swatmobility.com/" },
+  { logo: elctroneek, alt: "Electroneek", link: "https://electroneek.com/" },
+  { logo: pointOne, alt: "Point One", link: "https://pointonenav.com/" },
+  { logo: reverie, alt: "Reverie Labs", link: "https://www.reverielabs.com/" },
+  { logo: revl, alt: "Revl", link: "https://revl.com/" },
+  { logo: robotics, alt: "Robotics", link: "https://xrobotics.io/" },
+  { logo: prenav, alt: "Prenav", link: "https://www.prenav.com/" },
+  { logo: arloid, alt: "Arloid", link: "https://arloid.com/" },
+  { logo: creopop, alt: "Creopop", link: "https://creopop.com/" },
+  { logo: powerdot, alt: "Powerdot", link: "https://www.powerdot.com/" },
 ];
 
 const PortfolioBlock = () => {
@@ -49,9 +83,14 @@ const PortfolioBlock = () => {
                   {getCompaniesRow(companies, part as "up" | "down").map(
                     (item, idx) => {
                       return (
-                        <div className={styles.logoItem} key={item.alt + idx}>
+                        <a
+                          href={item.link}
+                          target="_blank"
+                          className={styles.logoItem}
+                          key={item.alt + idx}
+                        >
                           <Image src={item.logo} alt={item.alt} />
-                        </div>
+                        </a>
                       );
                     }
                   )}
--- frontend/src/components/PortfolioPage/PortfolioPage.tsx
+++ frontend/src/components/PortfolioPage/PortfolioPage.tsx
@@ -3,30 +3,61 @@ import mighty from "@images/mighty.png";
 import pointOne from "@images/pointone.png";
 import prenav from "@images/prenav.png";
 import solugen from "@images/solugen.png";
-import upcodes from "@images/upcodes.png";
 import revl from "@images/revl.png";
 import boom from "@images/boom.png";
-import vantageRobotics from "@images/vantage-robotics.png";
+import refocus from "@images/refocus.png";
+import alcatraz from "@images/alcatraz.png";
+import eightSleep from "@images/8sleep.png";
+import swat from "@images/swat.png";
+import elctroneek from "@images/electroneek.png";
+import reverie from "@images/reverie.png";
+import robotics from "@images/robotics.png";
+import arloid from "@images/arloid.png";
+import creopop from "@images/creopop.png";
+import powerdot from "@images/powerdot.png";
 import Image from "next/image";
 import Link from "next/link";
 
+// const logos = [
+//   { logo: mighty, link: "/", alt: "Mighty Buildings" },
+//   { logo: pointOne, link: "/", alt: "Point One" },
+//   { logo: prenav, link: "/", alt: "Prenav" },
+//   { logo: solugen, link: "/", alt: "Solugen" },
+//   { logo: upcodes, link: "/", alt: "Upcodes" },
+//   { logo: revl, link: "/", alt: "Revl" },
+//   { logo: boom, link: "/", alt: "Boom" },
+//   { logo: vantageRobotics, link: "/", alt: "Vantage Robotics" },
+//   { logo: mighty, link: "/", alt: "Mighty Buildings" },
+//   { logo: pointOne, link: "/", alt: "Point One" },
+//   { logo: prenav, link: "/", alt: "Prenav" },
+//   { logo: solugen, link: "/", alt: "Solugen" },
+//   { logo: upcodes, link: "/", alt: "Upcodes" },
+//   { logo: revl, link: "/", alt: "Revl" },
+//   { logo: boom, link: "/", alt: "Boom" },
+//   { logo: vantageRobotics, link: "/", alt: "Vantage Robotics" },
+// ];
+
 const logos = [
-  { logo: mighty, link: "/", alt: "Mighty Buildings" },
-  { logo: pointOne, link: "/", alt: "Point One" },
-  { logo: prenav, link: "/", alt: "Prenav" },
-  { logo: solugen, link: "/", alt: "Solugen" },
-  { logo: upcodes, link: "/", alt: "Upcodes" },
-  { logo: revl, link: "/", alt: "Revl" },
-  { logo: boom, link: "/", alt: "Boom" },
-  { logo: vantageRobotics, link: "/", alt: "Vantage Robotics" },
-  { logo: mighty, link: "/", alt: "Mighty Buildings" },
-  { logo: pointOne, link: "/", alt: "Point One" },
-  { logo: prenav, link: "/", alt: "Prenav" },
-  { logo: solugen, link: "/", alt: "Solugen" },
-  { logo: upcodes, link: "/", alt: "Upcodes" },
-  { logo: revl, link: "/", alt: "Revl" },
-  { logo: boom, link: "/", alt: "Boom" },
-  { logo: vantageRobotics, link: "/", alt: "Vantage Robotics" },
+  { logo: refocus, alt: "Refocus", link: "https://refocus.me/" },
+  { logo: alcatraz, alt: "Alcatraz AI", link: "https://www.alcatraz.ai/" },
+  {
+    logo: mighty,
+    alt: "Mighty Buildings",
+    link: "https://www.mightybuildings.com/",
+  },
+  { logo: boom, alt: "Boom", link: "https://boomsupersonic.com/" },
+  { logo: solugen, alt: "Solugen", link: "https://solugen.com/" },
+  { logo: eightSleep, alt: "Eight Sleep", link: "https://www.eightsleep.com/" },
+  { logo: swat, alt: "Swat", link: "https://www.swatmobility.com/" },
+  { logo: elctroneek, alt: "Electroneek", link: "https://electroneek.com/" },
+  { logo: pointOne, alt: "Point One", link: "https://pointonenav.com/" },
+  { logo: reverie, alt: "Reverie Labs", link: "https://www.reverielabs.com/" },
+  { logo: revl, alt: "Revl", link: "https://revl.com/" },
+  { logo: robotics, alt: "Robotics", link: "https://xrobotics.io/" },
+  { logo: prenav, alt: "Prenav", link: "https://www.prenav.com/" },
+  { logo: arloid, alt: "Arloid", link: "https://arloid.com/" },
+  { logo: creopop, alt: "Creopop", link: "https://creopop.com/" },
+  { logo: powerdot, alt: "Powerdot", link: "https://www.powerdot.com/" },
 ];
 
 const PortfolioPage = () => {
@@ -47,6 +78,7 @@ const PortfolioPage = () => {
                 className={styles.logoItem}
                 key={item.alt + idx}
                 href={item.link}
+                target="_blank"
               >
                 <Image src={item.logo} alt={item.alt} />
               </Link>
