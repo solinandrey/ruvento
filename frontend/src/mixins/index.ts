8fd86551a87c5d43a5ab969c9dafda352423ff4e
Andrey Solin <a.solin@warpa.ru>
2023-04-14 19:09:44 +0400

    fix plugin

--- frontend/src/mixins/index.ts
+++ frontend/src/mixins/index.ts
@@ -2,7 +2,7 @@ export const urlBuilder = (url: string) => {
   const { NEXT_PUBLIC_STRAPI_API_URL } = process.env;
   const env = process.env.NODE_ENV;
   if (env == "development") {
-    return 'http://localhost:1337' + url
+    return 'http://localhost:1337/' + url
   } else if (env == "production") {
     return NEXT_PUBLIC_STRAPI_API_URL + url;
   }
