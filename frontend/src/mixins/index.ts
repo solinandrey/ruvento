2464bd482ad033b736e479c7336379a0d0937aa6
Andrey Solin <a.solin@warpa.ru>
2023-04-17 21:45:05 +0400

    trying to fix fall 2

--- frontend/src/mixins/index.ts
+++ frontend/src/mixins/index.ts
@@ -1,4 +1,5 @@
 export const urlBuilder = (url: string) => {
+  if (!url) return
   const { NEXT_PUBLIC_STRAPI_API_URL } = process.env;
   const env = process.env.NODE_ENV;
   const urlCorrected = url.substring(0,1) === '/' ? url.substring(1) : url;
