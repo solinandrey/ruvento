156644184cf84152e31178dda33021005320589c
Andrey Solin <a.solin@warpa.ru>
2023-06-06 15:34:55 +0400

    prefix fix

--- frontend/src/mixins/index.ts
+++ frontend/src/mixins/index.ts
@@ -2,7 +2,7 @@ export const urlBuilder = (url: string): string => {
   // const { NEXT_PUBLIC_STRAPI_API_URL } = process.env;
   const NEXT_PUBLIC_STRAPI_API_URL = 'https://lobster-app-iw9uv.ondigitalocean.app/';
   const env = process.env.NODE_ENV;
-  const prefix = env === 'production' ? NEXT_PUBLIC_STRAPI_API_URL : env === 'development' ? 'http://localhost:1337/' : ''
+  const prefix = env === 'production' ? '' : env === 'development' ? 'http://localhost:1337/' : ''
   if (!url) return prefix || ''
   console.log(url, prefix, 'url')
   const urlCorrected = url.substring(0,1) === '/' ? url.substring(1) : url;
