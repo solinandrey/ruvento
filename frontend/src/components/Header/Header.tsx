6b627338ace05ab6209da81d62f72071d24ff4e2
Andrey Solin <a.solin@warpa.ru>
2023-06-06 19:36:11 +0400

    navigation main page

--- frontend/src/components/Header/Header.tsx
+++ frontend/src/components/Header/Header.tsx
@@ -2,6 +2,8 @@ import Link from "next/link";
 import Image from "next/image";
 import styles from "./Header.module.scss";
 import Logo from "@src/assets/images/logotype.svg";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 const headerLinks = [
   {
@@ -19,15 +21,32 @@ const headerLinks = [
 ];
 
 const Header = () => {
+  const router = useRouter();
+  useEffect(() => {
+    console.log(router, 'router')
+  },[router]) 
   return (
     <div className={styles.header}>
       <Link href="/" className={styles.logo}>
         <Logo />
       </Link>
       <div className={styles.links}>
+        <Link
+          key={`header-link-3`}
+          href="/"
+          className={`${styles.linkItem} ${
+            router.pathname === "/" ? styles.hidden : ""
+          }`}
+        >
+          {"Main page"}
+        </Link>
         {headerLinks.map((link, idx) => {
           return (
-            <Link key={`header-link-${idx}`} href={link.link} className={styles.linkItem}>
+            <Link
+              key={`header-link-${idx}`}
+              href={link.link}
+              className={styles.linkItem}
+            >
               {link.label}
             </Link>
           );
