05f5f7d3ba0eb9d0da61f244c68f35c1501b1c3e
Andrey Solin <a.solin@warpa.ru>
2023-06-06 15:50:44 +0400

    changed pattern

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,8 +3,7 @@ const path = require('path');
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    loader: "default",
-    domains: ["https://ruvento.s3.ap-southeast-1.amazonaws.com/"],
+    domains: ["ruvento.s3.ap-southeast-1.amazonaws.com"],
   },
   sassOptions: {
     includePaths: [path.join(__dirname, 'styles')],
