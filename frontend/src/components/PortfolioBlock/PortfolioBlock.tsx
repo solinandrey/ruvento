0a2a121fa92a79d7f9c57c1642eb8248efc27c82
Andrey Solin <a.solin@warpa.ru>
2023-06-12 17:48:04 +0400

    new marquee

--- frontend/src/components/PortfolioBlock/PortfolioBlock.tsx
+++ frontend/src/components/PortfolioBlock/PortfolioBlock.tsx
@@ -1,64 +1,13 @@
-import Image, { StaticImageData } from "next/image";
+import Image from "next/image";
+import Marquee from "react-fast-marquee";
 
 import styles from "./PortfolioBlock.module.scss";
 import { urlBuilder } from "@src/mixins";
-import mighty from "@images/mighty.png";
-import pointOne from "@images/pointone.png";
-import prenav from "@images/prenav.png";
-import solugen from "@images/solugen.png";
-import upcodes from "@images/upcodes.png";
-import revl from "@images/revl.png";
-import boom from "@images/boom.png";
-import vantageRobotics from "@images/vantage-robotics.png";
-import refocus from "@images/refocus.png";
-import alcatraz from "@images/alcatraz.png";
-import eightSleep from "@images/8sleep.png";
-import swat from "@images/swat.png";
-import elctroneek from "@images/electroneek.png";
-import reverie from "@images/reverie.png";
-import robotics from "@images/robotics.png";
-import arloid from "@images/arloid.png";
-import creopop from "@images/creopop.png";
-import powerdot from "@images/powerdot.png";
 
 interface CompanyLogo {
   logo: { data: { attributes: { url: string } } };
   link?: string;
 }
-
-// const companies: CompanyLogo[] = [
-//   { logo: mighty, alt: "Mighty Buildings" },
-//   { logo: pointOne, alt: "Point One" },
-//   { logo: prenav, alt: "Prenav" },
-//   { logo: solugen, alt: "Solugen" },
-//   { logo: upcodes, alt: "Upcodes" },
-//   { logo: revl, alt: "Revl" },
-//   { logo: boom, alt: "Boom" },
-//   { logo: vantageRobotics, alt: "Vantage Robotics" },
-// ];
-
-// const companies: CompanyLogo[] = [
-//   { logo: refocus, alt: "Refocus", link: "https://refocus.me/" },
-//   { logo: boom, alt: "Boom", link: "https://boomsupersonic.com/" },
-//   { logo: solugen, alt: "Solugen", link: "https://solugen.com/" },
-//   { logo: eightSleep, alt: "Eight Sleep", link: "https://www.eightsleep.com/" },
-//   {
-//     logo: mighty,
-//     alt: "Mighty Buildings",
-//     link: "https://www.mightybuildings.com/",
-//   },
-//   { logo: alcatraz, alt: "Alcatraz AI", link: "https://www.alcatraz.ai/" },
-//   { logo: swat, alt: "Swat", link: "https://www.swatmobility.com/" },
-//   { logo: elctroneek, alt: "Electroneek", link: "https://electroneek.com/" },
-//   { logo: pointOne, alt: "Point One", link: "https://pointonenav.com/" },
-//   { logo: reverie, alt: "Reverie Labs", link: "https://www.reverielabs.com/" },
-//   { logo: revl, alt: "Revl", link: "https://revl.com/" },
-//   { logo: robotics, alt: "Robotics", link: "https://xrobotics.io/" },
-//   { logo: prenav, alt: "Prenav", link: "https://www.prenav.com/" },
-//   { logo: arloid, alt: "Arloid", link: "https://arloid.com/" },
-//   { logo: creopop, alt: "Creopop", link: "https://creopop.com/" },
-//   { logo: powerdot, alt: "Powerdot", link: "https://www.powerdot.com/" },
-// ];
 
 interface Props {
   companies: CompanyLogo[];
@@ -84,7 +33,7 @@ const PortfolioBlock = ({ companies }: Props) => {
         <div className={styles.title}>our portfolio</div>
         {companies && companies.length && (
           <div className={styles.logos}>
-            {["up", "down"].map((part) => (
+            {/* {["up", "down"].map((part) => (
               <div className={`${styles.logoRow} ${styles[part]}`} key={part}>
                 {[1, 2].map((_, i) => (
                   <div className={styles.runnigRow} key={`dup${part}${i}`}>
@@ -99,7 +48,9 @@ const PortfolioBlock = ({ companies }: Props) => {
                             rel="noreferrer"
                           >
                             <Image
-                              src={urlBuilder(item?.logo?.data?.attributes?.url)}
+                              src={urlBuilder(
+                                item?.logo?.data?.attributes?.url
+                              )}
                               alt={item.link || ""}
                               width="500"
                               height="500"
@@ -111,7 +62,33 @@ const PortfolioBlock = ({ companies }: Props) => {
                   </div>
                 ))}
               </div>
-            ))}
+            ))} */}
+            {(["left", "right"] as ("left" | "right")[]).map((dir) => {
+              return (
+                <Marquee direction={dir}>
+                  <div className={styles.runnigRow} key={`dup`}>
+                    {companies.map((item, idx) => {
+                      return (
+                        <a
+                          href={item.link}
+                          target="_blank"
+                          className={styles.logoItem}
+                          key={(item.link || "") + idx}
+                          rel="noreferrer"
+                        >
+                          <Image
+                            src={urlBuilder(item?.logo?.data?.attributes?.url)}
+                            alt={item.link || ""}
+                            width="500"
+                            height="500"
+                          />
+                        </a>
+                      );
+                    })}
+                  </div>
+                </Marquee>
+              );
+            })}
           </div>
         )}
       </div>
